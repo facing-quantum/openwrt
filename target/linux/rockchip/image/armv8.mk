d278940a1573d5c3a216b5fdbff0b1c89e8e9f7e
Corey Moyer <cronmod.dev@gmail.com>
2023-04-18 10:23:46 -0400

    rockchip: advertise 2500Mbps on ETH2/ETH3 for LinkStar H68K
    - The RealTek 8152 driver does not advertise 2500Mbps, this script will use ethtool to force it.

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -7,7 +7,7 @@ define Device/linkstar_common
   SOC := rk3568
   UBOOT_DEVICE_NAME := opc-h68k-rk3568
   IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r5s | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-mt7921e kmod-r8125 kmod-usb-serial-cp210x wpad-openssl
+  DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-mt7921e kmod-r8125 kmod-usb-serial-cp210x wpad-openssl ethtool
 endef
 
 define Device/linkstar_opc-h68k
